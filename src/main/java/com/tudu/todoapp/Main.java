8bb6ce48781866e89b393ceb211520c28a78ff5e
yoanpetrov02 <87146784+yoanpetrov02@users.noreply.github.com>
2023-11-15 12:34:01 +0200

    Set up Main class and logging.

--- src/main/java/com/tudu/todoapp/Main.java
+++ src/main/java/com/tudu/todoapp/Main.java
@@ -1,4 +1,14 @@
 package com.tudu.todoapp;
 
+import org.apache.log4j.BasicConfigurator;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
+
+    public static void main(String[] args) {
+        BasicConfigurator.configure(); // logging
+        SpringApplication.run(Main.class, args);
+    }
 }
