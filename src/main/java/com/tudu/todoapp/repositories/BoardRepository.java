c995a9ba54236a07729caf273d26954581202629
yoanpetrov02 <87146784+yoanpetrov02@users.noreply.github.com>
2023-11-28 18:40:30 +0200

    Create all repositories.

---
+++ src/main/java/com/tudu/todoapp/repositories/BoardRepository.java
@@ -0,0 +1,9 @@
+package com.tudu.todoapp.repositories;
+
+import com.tudu.todoapp.entities.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BoardRepository extends JpaRepository<Long, Board> {
+}
