2d6b463675290e9b573d589a68b27dc00b8f32c1
Yoan Petrov <87146784+yoanpetrov02@users.noreply.github.com>
2023-11-30 16:11:04 +0200

    Revert "Implement UserService."

--- src/main/java/com/tudu/todoapp/Main.java
+++ src/main/java/com/tudu/todoapp/Main.java
@@ -3,9 +3,8 @@ package com.tudu.todoapp;
 import org.apache.log4j.BasicConfigurator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
--- src/main/java/com/tudu/todoapp/entities/User.java
+++ src/main/java/com/tudu/todoapp/entities/User.java
@@ -20,7 +20,7 @@ public class User {
     @GeneratedValue
     @Column(name = "user_id")
     private Long userId;
-    @Column(name = "display_name", unique = true)
+    @Column(name = "display_name")
     private String displayName;
 
     @OneToOne(cascade = CascadeType.ALL)
