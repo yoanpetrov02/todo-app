8fcffc2b760339cf967ce20d14b12e3399c87f0b
yoanpetrov02 <87146784+yoanpetrov02@users.noreply.github.com>
2023-11-30 15:31:25 +0200

    Add a pagination method to UserRepository.
    The findUsers() method can have a Pageable passed to it, which describes which page to return and how many items should be on the page.

--- src/main/java/com/tudu/todoapp/repositories/UserRepository.java
+++ src/main/java/com/tudu/todoapp/repositories/UserRepository.java
@@ -1,9 +1,14 @@
 package com.tudu.todoapp.repositories;
 
 import com.tudu.todoapp.entities.User;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    List<User> findUsers(Pageable pageable);
 }
