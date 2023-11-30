962269afc1cc257809ad2a3d317c1d2b2cf94474
yoanpetrov02 <87146784+yoanpetrov02@users.noreply.github.com>
2023-11-30 16:06:05 +0200

    Implement and document UserService.

--- src/main/java/com/tudu/todoapp/repositories/UserRepository.java
+++ src/main/java/com/tudu/todoapp/repositories/UserRepository.java
@@ -1,7 +1,6 @@
 package com.tudu.todoapp.repositories;
 
 import com.tudu.todoapp.entities.User;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +8,4 @@ import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
-    List<User> findUsers(Pageable pageable);
 }
