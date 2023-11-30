4507c97e4eeddac54954a7a3f0fb430c1c7ab248
yoanpetrov02 <87146784+yoanpetrov02@users.noreply.github.com>
2023-11-30 16:06:42 +0200

    Disable spring security for the application until we get to implementing it.

--- src/main/java/com/tudu/todoapp/Main.java
+++ src/main/java/com/tudu/todoapp/Main.java
@@ -3,8 +3,9 @@ package com.tudu.todoapp;
 import org.apache.log4j.BasicConfigurator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class Main {
 
     public static void main(String[] args) {
