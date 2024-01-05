0e659c7f233652f3f36018cdb24be12c4dd10d4f
Silvio <stoikov126@gmail.com>
2024-01-05 09:40:31 +0200

    Create a controller for ToDoItem.

---
+++ src/main/java/com/tudu/todoapp/rest/controllers/ToDoItemController.java
@@ -0,0 +1,10 @@
+package com.tudu.todoapp.rest.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/todoitems")
+public class ToDoItemController {
+
+}
