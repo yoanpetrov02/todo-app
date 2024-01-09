42e9e17dcec4cc64e3a498c02e5527da958a2e2b
yoanpetrov02 <87146784+yoanpetrov02@users.noreply.github.com>
2024-01-09 17:12:16 +0200

    Fix some random problem in the query for users by display name.

--- src/main/java/com/tudu/todoapp/repositories/UserRepository.java
+++ src/main/java/com/tudu/todoapp/repositories/UserRepository.java
@@ -4,6 +4,8 @@ import com.tudu.todoapp.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 import java.util.Optional;
 
 
@@ -11,8 +13,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findUserByDisplayName(String displayName);
 
-    @Query("SELECT u FROM Users u WHERE lower(u.displayName) LIKE :#{#displayName == null || #displayName.isEmpty() ? '%' : #displayName+'%'}")
+    @Query("SELECT u FROM User u WHERE lower(u.displayName) LIKE :#{#displayName == null || #displayName.isEmpty() ? '%' : #displayName+'%'}")
     List<User> filterUsersByDisplayName(String displayName);
 }
-
-
