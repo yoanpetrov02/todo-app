79cb3edb2af3c72fab3ff3a130c94c7774bef12f
yoanpetrov02 <87146784+yoanpetrov02@users.noreply.github.com>
2024-01-09 17:42:34 +0200

    Set up swagger ui for the project.

--- src/main/java/com/tudu/todoapp/repositories/TodoListRepository.java
+++ src/main/java/com/tudu/todoapp/repositories/TodoListRepository.java
@@ -2,11 +2,15 @@ package com.tudu.todoapp.repositories;
 
 import com.tudu.todoapp.entities.TodoList;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import java.util.Optional;
+
+import java.util.List;
+
 @Repository
 public interface TodoListRepository extends JpaRepository<TodoList, Long> {
-    @Query("SELECT tl FROM todo_lists tl WHERE lower(tl.title) LIKE " +
+
+    @Query("SELECT tl FROM TodoList tl WHERE lower(tl.title) LIKE " +
         ":#{#title == null || #title.isEmpty() ? '%' : #title+'%'}")
-    List<TodoList> filterTodoListsByTitle(String title)
+    List<TodoList> filterTodoListsByTitle(String title);
 }
