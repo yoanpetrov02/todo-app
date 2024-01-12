8403ff58a6943d231589a2879ec61fbc3cafdb2f
Silvio <stoikov126@gmail.com>
2024-01-12 13:47:20 +0200

    Create UserDto.

---
+++ src/main/java/com/tudu/todoapp/dto/UserDto.java
@@ -0,0 +1,17 @@
+package com.tudu.todoapp.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+
+    @NotBlank
+    private String displayName;
+}
