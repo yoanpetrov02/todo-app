1cbefa592e57a227473dfa301383383780684932
Silvio <stoikov126@gmail.com>
2024-01-12 13:12:42 +0200

    Add constraints to UserAccountDto.

--- src/main/java/com/tudu/todoapp/dto/UserAccountDto.java
+++ src/main/java/com/tudu/todoapp/dto/UserAccountDto.java
@@ -1,6 +1,7 @@
 package com.tudu.todoapp.dto;
 
 import com.tudu.todoapp.security.Role;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,7 +13,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class UserAccountDto {
 
+    @NotBlank
     private String email;
+    @NotBlank
     private String password;
     private Role role;
 }
