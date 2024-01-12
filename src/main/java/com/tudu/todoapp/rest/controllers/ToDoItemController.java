3c57834294a67cd709a4a652254b4edfd97a7c92
Silvio <stoikov126@gmail.com>
2024-01-12 16:20:07 +0200

    Use mappers in controllers.

--- src/main/java/com/tudu/todoapp/rest/controllers/ToDoItemController.java
+++ src/main/java/com/tudu/todoapp/rest/controllers/ToDoItemController.java
@@ -11,7 +11,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 
