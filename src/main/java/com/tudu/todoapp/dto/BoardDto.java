092ed174be806dbccbfce8ff9c610acfc119f84b
Silvio <stoikov126@gmail.com>
2024-01-12 13:42:01 +0200

    Create BoardDto.

---
+++ src/main/java/com/tudu/todoapp/dto/BoardDto.java
@@ -0,0 +1,19 @@
+package com.tudu.todoapp.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class BoardDto {
+
+    @NotBlank
+    private String title;
+    @NotBlank
+    private String description;
+}
