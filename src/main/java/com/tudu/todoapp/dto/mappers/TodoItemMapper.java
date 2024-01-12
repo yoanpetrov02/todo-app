b95bd0583663caa7e552bd7ed2bb3a128c594fdc
Silvio <stoikov126@gmail.com>
2024-01-12 12:37:12 +0200

    Create TodoItem mapper to its DTO.

---
+++ src/main/java/com/tudu/todoapp/dto/mappers/TodoItemMapper.java
@@ -0,0 +1,15 @@
+package com.tudu.todoapp.dto.mappers;
+
+import com.tudu.todoapp.dto.TodoItemDTO;
+import com.tudu.todoapp.entities.TodoItem;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+import org.springframework.stereotype.Component;
+
+@Component
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface TodoItemMapper {
+    @Mapping(target="TodoItem", source = "TodoItemDTO")
+    TodoItem dtoToEntity(TodoItemDTO todoItemDTO);
+}
