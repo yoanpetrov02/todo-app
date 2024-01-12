340ad5997abb1c2de5849cb168d9ccd6cd3a5d17
Silvio <stoikov126@gmail.com>
2024-01-12 13:44:46 +0200

    Create BoardMapper.

---
+++ src/main/java/com/tudu/todoapp/dto/mappers/BoardMapper.java
@@ -0,0 +1,15 @@
+package com.tudu.todoapp.dto.mappers;
+
+import com.tudu.todoapp.dto.BoardDto;
+import com.tudu.todoapp.entities.Board;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+import org.springframework.stereotype.Component;
+
+@Component
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface BoardMapper {
+    @Mapping(target="Board", source = "BoardDTO")
+    Board dtoToEntity(BoardDto boardDto);
+}
